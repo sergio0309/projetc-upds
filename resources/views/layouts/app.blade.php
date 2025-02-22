13d397585493c1fb0202dc54cfc2698b24686474
sergio0309 <sergio.rodriguezaramayo@gmail.com>
2025-02-21 21:35:37 -0400

    detalles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,26 +28,11 @@
     <!-- custom Css-->
     <link href="{{asset('assets/css/custom.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
-
-    <!-- App favicon -->
-    {{-- <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
-    <!-- Layout config Js -->
-    <script src="{{asset('assets/js/layout.js')}}" type="js"></script>
-    <!-- Bootstrap Css -->
-    <link href="{{asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href="{{asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
-    <!-- custom Css-->
-    <link href="{{asset('assets/css/custom.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
-
     <link href="{{asset('assets/libs/sweetalert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css" />
-    <!--datatable css-->
     <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-    <!--datatable responsive css-->
+    !--datatable responsive css-->
     <link href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" /> --}}
+    <link href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -731,9 +716,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
     <script src="{{asset('assets/js/pages/datatables.init.js')}}"></script>
-
-    <script src="{{asset('assets/js/app.min.js')}}"></script>
-
 
 </body>
 
